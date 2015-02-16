651a63a98e098e7e41bf619236d0230be09a8dd1
Tyler Marshik <tmarshik@ucsc.edu>
2015-02-15 22:21:03 -0800

    Deleted useless comments

--- controllers/default.py
+++ controllers/default.py
@@ -1,14 +1,3 @@
-# -*- coding: utf-8 -*-
-# this file is released under public domain and you can use without limitations
-
-#########################################################################
-## This is a sample controller
-## - index is the default action of any application
-## - user is required for authentication and authorization
-## - download is for downloading files uploaded in the db (does streaming)
-## - api is an example of Hypermedia API support and access control
-#########################################################################
-
 def index():
     """
     example action using the internationalization operator T and flash
