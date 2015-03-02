2e70372120865823b92f17bbea4441fb6c5a5ce9
wmossman <wmossman@ucsc.edu>
2015-03-02 06:32:41 -0800

    created good html pages

--- controllers/default.py
+++ controllers/default.py
@@ -1,5 +1,6 @@
 import test
 
+
 def index():
     if auth.user:
         name = auth.user.first_name
@@ -8,19 +9,46 @@ def index():
         redirect(URL('default', 'login'))
     return dict()
 
-#login page
+@auth.requires_login()  # eventually this will probably have to be requires_membership(class.teachers) or something
+def create_test():
+    """
+    Test creation controller
+    If form succeeds, creates test and pushes it to test_list
+
+    Future:
+    Will be
+    Must be a teacher for course
+    :return:
+    """
+
+    return dict()
+
+
+def test_list():
+    """
+    List of Tests
+
+    Future: should be based on the classes you are in
+    :return:
+    """
+    return dict()
+
+
+# login page
 def login():
     response.flash = T("Login Page")
     form = auth.login()
     register_button = A('Register', _class='btn', _href=URL('default', 'register'))
     return dict(form=form, register_button=register_button)
 
-#register page
+
+# register page
 def register():
     response.flash = T("Register Page")
     form=auth.register()
     back_button = A('Back', _class='btn', _href=URL('default', 'login'))
     return dict(form=form, back_button=back_button)
+
 
 def user():
     """
